5e5a1c801eafe66d7d436794c0bcfabb7c83d3ad
pandaorb <allisonmacdonald@acm.org>
2016-07-24 14:45:05 -0500

    Added CREATE sql for patients and workers

---
+++ gen_code/sql/Worker.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Worker (
+	employeeId integer,
+	firstName varchar(20) NOT NULL,
+	lastName varchar(20) NOT NULL,
+	salary integer NOT NULL,
+	hireDate datetime NOT NULL,
+	PRIMARY KEY (employeeId)
+);
