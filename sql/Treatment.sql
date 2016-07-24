69c001dd810fd257783817734b631f59c036838d
John Cook <cookjohns@users.noreply.github.com>
2016-07-24 16:36:20 -0500

    Formatting

--- sql/Treatment.sql
+++ sql/Treatment.sql
@@ -1,6 +1,6 @@
 drop table if exists Treatment; 
 
-create table Treatment (
+CREATE TABLE Treatment (
    treatmentID integer,
    name varchar(20),
    isMedication bit, // return 1 for medication, 0 for procedure
