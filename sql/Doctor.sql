e49957dffaa56358827033eca5028e760185e1a2
austin chase minor <acm0055@auburn.edu>
2016-07-25 09:30:07 -0500

    fixed treatment giver in jst and also removed consulting flag since all doctors have consulting privaleges

--- sql/Doctor.sql
+++ sql/Doctor.sql
@@ -2,7 +2,6 @@ drop table if exists Doctor;
 
 CREATE TABLE Doctor (
 	employeeID integer,
-	consultingPrivilege byte,
    treatmentGiverID integer NOT NULL,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (employeeID) REFERENCES Worker.employeeID,
