f7b5c915af05691debdc26b56b15ae87abf2548f
austin chase minor <acm0055@auburn.edu>
2016-07-26 08:57:11 -0500

    fixed uniqueness constrants relating to treatmentgiver

--- sql/Nurse.sql
+++ sql/Nurse.sql
@@ -2,7 +2,7 @@ drop table if exists Nurse;
 
 CREATE TABLE Nurse (
 	employeeID integer,
-   treatmentGiverID integer NOT NULL,
+   treatmentGiverID integer NOT NULL UNIQUE,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (employeeID) REFERENCES Worker(employeeID),
    FOREIGN KEY (treatmentGiverID) REFERENCES TreatmentGiver(treatmentGiverID)
--- sql/Technician.sql
+++ sql/Technician.sql
@@ -2,7 +2,7 @@ drop table if exists Technician;
 
 CREATE TABLE Technician (
 	employeeId integer,
-   treatmentGiverID integer NOT NULL,
+   treatmentGiverID integer NOT NULL UNIQUE,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (employeeID) REFERENCES Worker(employeeID),
    FOREIGN KEY (treatmentGiverID) REFERENCES TreatmentGiver(treatmentGiverID)
