9e1c69837f2908b5afa5d95479f78cd77b767d5c
austin chase minor <acm0055@auburn.edu>
2016-07-27 16:47:59 -0500

    gave doctor admitting privileges. also created initial diagnoses jst

--- sql/Doctor.sql
+++ sql/Doctor.sql
@@ -3,6 +3,7 @@ drop table if exists Doctor;
 CREATE TABLE Doctor (
 	employeeID integer,
    treatmentGiverID integer NOT NULL UNIQUE,
+   admittingPrivilege integer NOT NULL,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (employeeID) REFERENCES Worker(employeeID),
    FOREIGN KEY (treatmentGiverID) REFERENCES TreatmentGiver(treatmentGiverID)
