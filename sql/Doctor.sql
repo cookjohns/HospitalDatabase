92c9e0b8f704b056afa81edd6b638d8a8dcee94f
austin chase minor <acm0055@auburn.edu>
2016-07-27 17:08:31 -0500

    Fixed doctor and diagnoses.

--- sql/Doctor.sql
+++ sql/Doctor.sql
@@ -3,7 +3,6 @@ drop table if exists Doctor;
 CREATE TABLE Doctor (
 	employeeID integer,
    treatmentGiverID integer NOT NULL UNIQUE,
-   admittingPrivilege integer NOT NULL,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (employeeID) REFERENCES Worker(employeeID),
    FOREIGN KEY (treatmentGiverID) REFERENCES TreatmentGiver(treatmentGiverID)
